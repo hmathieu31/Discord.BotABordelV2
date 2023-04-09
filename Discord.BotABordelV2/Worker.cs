371734ff3baeb67fb49b43f41e90d41a65e3ad75
Hugo MATHIEU <hugo.mathieu@exakis-nelite.com>
2023-04-09 10:41:34 +0200

    Implement Client connection and disconnection

--- Discord.BotABordelV2/Worker.cs
+++ Discord.BotABordelV2/Worker.cs
@@ -1,21 +1,42 @@
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+
 namespace Discord.BotABordelV2
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly DiscordClient _discordClient;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, DiscordClient discordClient)
         {
             _logger = logger;
+            _discordClient = discordClient;
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        public override async Task StartAsync(CancellationToken cancellationToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+            _discordClient.MessageCreated += OnMessageCreated;
+
+
+            await _discordClient.ConnectAsync();
+            _logger.LogInformation("Discord Client connected");
         }
+
+        private async Task OnMessageCreated(DiscordClient sender, MessageCreateEventArgs args)
+        {
+            if (args.Message.Content.Equals("ping"))
+                await args.Message.RespondAsync("Pong");
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _discordClient.DisconnectAsync();
+            _discordClient.Dispose();
+            _logger.LogInformation("Discord Client disconnected");
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+            => Task.CompletedTask;
     }
 }
