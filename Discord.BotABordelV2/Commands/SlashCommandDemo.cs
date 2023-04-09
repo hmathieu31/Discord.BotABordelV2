4935247d32aaa2d03c16feb89e6e121e6cf2608f
Hugo MATHIEU <hugo.mathieu@exakis-nelite.com>
2023-04-09 10:46:48 +0200

    Add demo commands

---
+++ Discord.BotABordelV2/Commands/SlashCommandDemo.cs
@@ -0,0 +1,23 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord.BotABordelV2.Commands;
+
+public class SlashCommandDemo : ApplicationCommandModule
+{
+    [SlashCommand("test", "Slash test command")]
+    public async Task TestCommand(InteractionContext context)
+    {
+        await context.CreateResponseAsync(
+            InteractionResponseType.ChannelMessageWithSource,
+            new DiscordInteractionResponseBuilder()
+                .WithContent("Wesh Wesh Canapèche")
+            );
+    }
+}
