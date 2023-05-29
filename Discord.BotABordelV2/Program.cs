08c92a059646ec073757a5914bbb6f3d5da20c76
Hugo MATHIEU <hugo.mathieu@exakis-nelite.com>
2023-05-29 20:00:28 +0200

    ➕ Add Azure App Configuration as provider in Prod

--- Discord.BotABordelV2/Program.cs
+++ Discord.BotABordelV2/Program.cs
@@ -1,112 +1,167 @@
+using System.Diagnostics;
+using System.Reflection;
+
+using Azure.Identity;
+
 using Discord.BotABordelV2.Configuration;
 using Discord.BotABordelV2.Interfaces;
 using Discord.BotABordelV2.Services;
 using Discord.BotABordelV2.Services.Media;
+
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.Lavalink;
 using DSharpPlus.Net;
 using DSharpPlus.SlashCommands;
 using DSharpPlus.VoiceNext;
+
 using Microsoft.Extensions.Options;
+
 using Serilog;
-using System.Reflection;
 
-namespace Discord.BotABordelV2
+namespace Discord.BotABordelV2;
+
+public static class Program
 {
-    public static class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            await Host.CreateDefaultBuilder(args)
-                .UseConsoleLifetime()
-                .ConfigureServices((builder, services) =>
-                {
-                    services.AddHostedService<BotABordelService>()
-                            .AddDiscordBotOptions(builder)
-                            .AddDiscordClient()
-                            .AddLavalink()
-                            .AddTransient<StreamingMediaService>()
-                            .AddTransient<LocalMediaService>()
-                            .AddSingleton<IGrandEntranceService, GrandEntrancesService>();
-                })
-                .UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration))
-                .RunConsoleAsync();
+        var builder = Host.CreateDefaultBuilder(args)
+                          .UseConsoleLifetime();
 
-            await Log.CloseAndFlushAsync();
-        }
+#if !DEBUG
+        // Add Azure App Configuration
+        builder.UseAzureAppConfiguration();
+#endif
 
-        private static IServiceCollection AddDiscordClient(this IServiceCollection services)
+        // Configure services
+        builder.ConfigureServices((context, services) =>
         {
-            services.AddSingleton((serviceProvider) =>
+            services.AddHostedService<BotABordelService>()
+                    .AddDiscordBotOptions(context)
+                    .AddDiscordClient()
+                    .AddLavalink()
+                    .AddTransient<StreamingMediaService>()
+                    .AddTransient<LocalMediaService>()
+                    .AddSingleton<IGrandEntranceService, GrandEntrancesService>();
+        });
+
+        // Configure logging
+        builder.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
+        await builder.Build().RunAsync();
+
+        await Log.CloseAndFlushAsync();
+    }
+
+    /// <summary>
+    /// Adds Azure App Configuration to the service collection.
+    /// </summary>
+    /// <param name="builder">The host builder object.</param>
+    /// <returns>A reference to this instance after the operation has completed.</returns>
+    private static IHostBuilder UseAzureAppConfiguration(this IHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration((context, cfg) =>
+        {
+            var settings = cfg.Build();
+            cfg.AddAzureAppConfiguration(options =>
             {
-                var configuration = serviceProvider.GetRequiredService<IOptions<DiscordBot>>().Value;
-                var discordClient = new DiscordClient(new DiscordConfiguration
-                {
-                    Token = configuration.Token,
-                    TokenType = TokenType.Bot,
-                    Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents,
-                    LoggerFactory = new LoggerFactory().AddSerilog(),
-                    MinimumLogLevel = LogLevel.Trace,
-                    LogUnknownEvents = true
-                });
-                var commands = discordClient.UseCommandsNext(new CommandsNextConfiguration()
-                {
-                    StringPrefixes = new[] { "!" }
-                });
-                var slash = discordClient.UseSlashCommands(new SlashCommandsConfiguration
-                {
-                    Services = serviceProvider
-                });
+                var connectionString = settings.GetConnectionString("AppConfig");
+                if (connectionString is not null)
+                    options.Connect(connectionString);
+                else
+                    Console.WriteLine("Azure App Config was not configured");
+            });
+        });
 
-                discordClient.UseVoiceNext();
-                commands.RegisterCommands(Assembly.GetExecutingAssembly());
-                slash.RegisterCommands(Assembly.GetExecutingAssembly());
+        return builder;
+    }
 
-                return discordClient;
+    /// <summary>
+    /// Adds a Discord client to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <returns>A reference to this instance after the operation has completed.</returns>
+    private static IServiceCollection AddDiscordClient(this IServiceCollection services)
+    {
+        services.AddSingleton((serviceProvider) =>
+        {
+            var configuration = serviceProvider.GetRequiredService<IOptions<DiscordBot>>().Value;
+            var discordClient = new DiscordClient(new DiscordConfiguration
+            {
+                Token = configuration.Token,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents,
+                LoggerFactory = new LoggerFactory().AddSerilog(),
+                MinimumLogLevel = LogLevel.Trace,
+                LogUnknownEvents = true
+            });
+            var commands = discordClient.UseCommandsNext(new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { "!" }
+            });
+            var slash = discordClient.UseSlashCommands(new SlashCommandsConfiguration
+            {
+                Services = serviceProvider
             });
 
-            return services;
-        }
+            discordClient.UseVoiceNext();
+            commands.RegisterCommands(Assembly.GetExecutingAssembly());
+            slash.RegisterCommands(Assembly.GetExecutingAssembly());
 
-        private static IServiceCollection AddLavalink(this IServiceCollection services)
+            return discordClient;
+        });
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds Lavalink to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <returns>A reference to this instance after the operation has completed.</returns>
+    private static IServiceCollection AddLavalink(this IServiceCollection services)
+    {
+        services.AddSingleton((serviceProvider) =>
         {
-            services.AddSingleton((serviceProvider) =>
-            {
-                return serviceProvider.GetRequiredService<DiscordClient>().UseLavalink();
-            });
-            services.AddSingleton((serviceProvider) =>
-            {
-                var options = serviceProvider.GetRequiredService<IOptions<Lavalink>>().Value;
-                var endpoint = new ConnectionEndpoint
-                {
-                    Hostname = options.Host, // From your server configuration.
-                    Port = options.Port // From your server configuration
-                };
-
-                return new LavalinkConfiguration
-                {
-                    Password = options.Password, // From your server configuration.
-                    RestEndpoint = endpoint,
-                    SocketEndpoint = endpoint
-                };
-            });
-
-            return services;
-        }
-
-        private static IServiceCollection AddDiscordBotOptions(this IServiceCollection services, HostBuilderContext context)
+            return serviceProvider.GetRequiredService<DiscordClient>().UseLavalink();
+        });
+        services.AddSingleton((serviceProvider) =>
         {
-            var botSection = context.Configuration.GetRequiredSection("DiscordBot");
-            var lavalinkSection = context.Configuration.GetRequiredSection("Lavalink");
+            var options = serviceProvider.GetRequiredService<IOptions<Lavalink>>().Value;
+            var endpoint = new ConnectionEndpoint
+            {
+                Hostname = options.Host, // From your server configuration.
+                Port = options.Port // From your server configuration
+            };
 
-            services.Configure<DiscordBot>(botSection)
-                    .Configure<Lavalink>(lavalinkSection);
+            return new LavalinkConfiguration
+            {
+                Password = options.Password, // From your server configuration.
+                RestEndpoint = endpoint,
+                SocketEndpoint = endpoint
+            };
+        });
 
-            botSection.Bind(new DiscordBot());
-            lavalinkSection.Bind(new Lavalink());
+        return services;
+    }
 
-            return services;
-        }
+    /// <summary>
+    /// Configure options for the Discord bot.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <param name="context">The host builder context.</param>
+    /// <returns>A reference to this instance after the operation has completed.</returns>
+    private static IServiceCollection AddDiscordBotOptions(this IServiceCollection services, HostBuilderContext context)
+    {
+        var botSection = context.Configuration.GetRequiredSection("DiscordBot");
+        var lavalinkSection = context.Configuration.GetRequiredSection("Lavalink");
+
+        services.Configure<DiscordBot>(botSection)
+                .Configure<Lavalink>(lavalinkSection);
+
+        botSection.Bind(new DiscordBot());
+        lavalinkSection.Bind(new Lavalink());
+
+        return services;
     }
 }
