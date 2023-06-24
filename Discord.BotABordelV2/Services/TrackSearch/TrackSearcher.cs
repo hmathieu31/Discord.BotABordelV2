4173409f7ecb89a68fee7cc2b6c2941fb7801698
Hugo MATHIEU <hugo.mathieu@exakis-nelite.com>
2023-06-24 10:55:05 +0200

    ♻️ Refactor StreamingMediaService to use a TrackSearcher to find track

---
+++ Discord.BotABordelV2/Services/TrackSearch/TrackSearcher.cs
@@ -0,0 +1,20 @@
+﻿using DSharpPlus.Lavalink;
+
+namespace Discord.BotABordelV2.Services.TrackSearch;
+
+public class TrackSearcher
+{
+    public async Task<LavalinkTrack?> SearchTrackAsync(LavalinkGuildConnection connection, string trackQuery)
+    {
+        var loadResults = await connection.GetTracksAsync(trackQuery);
+        //If something went wrong on Lavalink's end
+        if (loadResults.LoadResultType == LavalinkLoadResultType.LoadFailed
+            //or it just couldn't find anything.
+            || loadResults.LoadResultType == LavalinkLoadResultType.NoMatches)
+        {
+            return null;
+        }
+
+        return loadResults.Tracks.FirstOrDefault();
+    }
+}
